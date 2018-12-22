fb9a725e211729074e0ab2394565f02532ff3d4b
Matouš Kulhan <KulhanMatous@sssvt.cz>
2018-12-22 19:23:44 +0100

    Initial commit

---
+++ app/src/main/java/ko/kufr/m77m77/chatandroidclient/MainActivity.java
@@ -0,0 +1,13 @@
+package ko.kufr.m77m77.chatandroidclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
